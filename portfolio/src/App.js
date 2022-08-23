bd9457a990d528df8a04b79af7f9b4fbfd52bae7
brennuck <brennuck@gmail.com>
2022-08-23 12:05:42 -0600

    update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import "./styles.css";
+import namelogo from "./images/namelogo.png";
 
 function App() {
-    return <div className="App">hi (:</div>;
+    return (
+        <div className="App">
+            <header>
+                <img src={namelogo} alt="kayla nuckols" className="nameLogo" />
+            </header>
+            <div className="body">
+                <h1 className="bigheader">
+                    Sed ut perspiciatis unde omnis iste natus
+                </h1>
+                <a href="mailto:kaylanucks@gmail.com" className="emailButton">
+                    Email Me
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
